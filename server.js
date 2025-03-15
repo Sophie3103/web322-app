b3f93213e79ec5f661a9fbf852f9287586428c58
Sofiia <sparkhomenko@myseneca.ca>
2025-03-15 19:54:42 -0400

    Assignment 3 updates

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 /*********************************************************************************
-WEB322 – Assignment 02
+WEB322 – Assignment 03
 I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
 No part of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 
 Name: Sofiia Parkhomenko
 Student ID: 123054215
-Date: 02/16/2025
-Vercel Web App URL: 
-GitHub Repository URL: 
+Date: 03/15/2025
+Vercel Web App URL: https://vercel.com/sophie3103s-projects/web322-app
+GitHub Repository URL: https://github.com/Sophie1303/web322-app.git
 
 ********************************************************************************/
-
 const express = require("express");
 const path = require("path");
 const storeService = require("./store-service");
@@ -20,6 +19,19 @@ const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
+cloudinary.config({
+  cloud_name: "daxpgprze",
+  api_key: "999964125187316",
+  api_secret: "BmpyIgGUILXl8EGvsJJYQJtBfqg",
+  secure: true
+});
+const upload = multer(); 
 
 app.get("/", (req, res) => {
   res.redirect("/about");
@@ -40,17 +52,6 @@ app.get("/shop", (req, res) => {
     });
 });
 
-app.get("/items", (req, res) => {
-  storeService
-    .getAllItems()
-    .then((allItems) => {
-      res.json(allItems);
-    })
-    .catch((err) => {
-      res.json({ message: err });
-    });
-});
-
 app.get("/categories", (req, res) => {
   storeService
     .getCategories()
@@ -61,6 +62,93 @@ app.get("/categories", (req, res) => {
       res.json({ message: err });
     });
 });
+
+app.get("/items", (req, res) => {
+  if (req.query.category) {
+    storeService
+      .getItemsByCategory(req.query.category)
+      .then((filteredItems) => {
+        res.json(filteredItems);
+      })
+      .catch((err) => {
+        res.json({ message: err });
+      });
+  }
+  else if (req.query.minDate) {
+    storeService
+      .getItemsByMinDate(req.query.minDate)
+      .then((filteredItems) => {
+        res.json(filteredItems);
+      })
+      .catch((err) => {
+        res.json({ message: err });
+      });
+  }
+  else {
+    storeService
+      .getAllItems()
+      .then((allItems) => {
+        res.json(allItems);
+      })
+      .catch((err) => {
+        res.json({ message: err });
+      });
+  }
+});
+app.get("/item/:id", (req, res) => {
+  storeService
+    .getItemById(req.params.id)
+    .then((item) => {
+      res.json(item);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+app.post("/items/add", upload.single("featureImage"), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+    async function upload(req) {
+      let result = await streamUpload(req);
+      return result;
+    }
+    upload(req)
+      .then((uploaded) => {
+        processItem(uploaded.url);
+      })
+      .catch((err) => {
+        console.log(err);
+        processItem(""); 
+      });
+  } else {
+    processItem("");
+  }
+
+  function processItem(imageUrl) {
+    req.body.featureImage = imageUrl;
+
+    storeService.addItem(req.body)
+      .then(() => {
+        res.redirect("/items"); 
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  }
+});
+
 
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
--- store-service.js
+++ store-service.js
@@ -1,13 +1,13 @@
 /*********************************************************************************
-WEB322 – Assignment 02
+WEB322 – Assignment 03
 I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
 No part of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 
 Name: Sofiia Parkhomenko
 Student ID: 123054215
-Date: 02/16/2025
-Vercel Web App URL: 
-GitHub Repository URL: 
+Date: 03/15/2025
+Vercel Web App URL: https://vercel.com/sophie3103s-projects/web322-app
+GitHub Repository URL: https://github.com/Sophie1303/web322-app.git 
 
 ********************************************************************************/
 
@@ -78,9 +78,60 @@ function getCategories() {
   });
 }
 
+function getItemsByCategory(category) {
+  return new Promise((resolve, reject) => {
+    const filtered = items.filter((item) => item.category == category);
+    if (filtered.length > 0) resolve(filtered);
+    else reject("no results returned");
+  });
+}
+
+function getItemsByMinDate(minDateStr) {
+  return new Promise((resolve, reject) => {
+    const filtered = items.filter(
+      (item) => new Date(item.postDate) >= new Date(minDateStr)
+    );
+    if (filtered.length > 0) resolve(filtered);
+    else reject("no results returned");
+  });
+}
+
+function getItemById(id) {
+  return new Promise((resolve, reject) => {
+    const found = items.find((itm) => itm.id == id);
+    if (found) resolve(found);
+    else reject("no result returned");
+  });
+}
+function addItem(itemData) {
+  return new Promise((resolve, reject) => {
+    itemData.published = itemData.published ? true : false;
+
+    if (itemData.category) {
+      itemData.category = parseInt(itemData.category);
+    }
+
+    if (!itemData.postDate) {
+      itemData.postDate = new Date().toISOString().split("T")[0];
+    }
+
+    itemData.id = items.length + 1;
+
+    items.push(itemData);
+
+    resolve(itemData);
+  });
+}
+
+
+
 module.exports = {
   initialize,
   getAllItems,
   getPublishedItems,
-  getCategories
+  getCategories,
+  addItem,
+  getItemsByCategory,
+  getItemsByMinDate,
+  getItemById
 };
